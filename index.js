ff7796c85b0a930407b06e5182013832540660bb
Lance Gorton <lance@Lances-MacBook-Air.local>
2023-12-05 09:29:23 -0600

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const names = ['foo', 'bar', 'bazz', 'quq', 'fizz', 'buzz'];
+const colors = ['red', 'blue', 'yellow', 'green'];
+const productsList = window.document.querySelector('#productsList');
+console.log(productsList);
+
+function generateRandom(){
+    const nameIdx = Math.floor(Math.random()*names.length);
+    const name = names[nameIdx];
+    const colorIdx = Math.floor(Math.random()*colors.length);
+    const color = colors[colorIdx];
+    const cost = Math.ceil(Math.random() * 10);
+
+ return{
+   name, 
+   color, 
+   cost
+ };
+}
+
+function render(){
+    const lis = products.map(function(product){
+       return `<li>${product.name} is ${product.color} and costs $${product.cost}</li>`;
+    }).join('');
+    productsList.innerHTML = lis;
+}
+
+const products = [];
+products.push(generateRandom());
+products.push(generateRandom());
+
+render();
